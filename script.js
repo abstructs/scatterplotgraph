b7d250be480b6322ceae1906e0f97d4f4730ee56
polygoning <polygoning@gmail.com>
2016-07-08 14:33:07 -0400

    add links, labels, legend and finish

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ createChart = function(data) {
    return d.Place
   });
 
-  timeMax.setSeconds(10)
+  timeMax.setSeconds(10);
   timeMax.setMinutes(timeMax.getMinutes() + 1); // lets us add more to the x axis for better readability
 
   var xAxisScale = d3.scaleTime()
@@ -59,9 +59,20 @@ createChart = function(data) {
       .attr('width', width)
   var chart = d3.select('body').select('svg');
 
+  var div = d3.select("body").append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
+
       chart.selectAll('.chart')
       .data(data).enter()
       .append('g')
+      .append('a')
+      .attr('xlink:href', function(d, i){
+        if (d.URL) {
+          return d.URL
+        }
+      })
+      .attr('target', '__blank')
       .append('circle')
       .attr('cx', function(d, i) {
         var date = new Date();
@@ -79,21 +90,88 @@ createChart = function(data) {
         else {
           return 'red'
         }
+      }) // alternates color based on whether doping is true or not
+      .on("mouseover", function(d) {
+        var doping = function() {
+          if (d.Doping) {
+            return '</br></br>' + d.Doping
+          }
+          else {
+            return ''
+          }
+        }(); // returns a newline with doping info if not false/empty string
+        div.transition()
+          .duration(200)
+          .style("opacity", .9);
+        div.html(d.Name + ': ' + d.Nationality + '</br>' + 'Year: ' + d.Year + ', Time: ' + d.Time + doping) // string parsing for tooltip
+        .style("left", "325px")
+        .style("top", "150px");
       })
-      //.text(function(d, i){ console.log(d.Name)})
+      .on("mouseout", function(d) { // on mouse out set opacity to 0 over 500ms
+        div.transition()
+          .duration(500)
+          .style("opacity", 0);
+      });
 
-  d3.select("body").select("svg")
-    .append("g")
-    .attr("width", width)
-    .attr("transform", "translate(70, 375)")
-    .call(bottomAxis)
+  d3.select("svg").append("text")  // create the title for the graph
+      .attr("x", width / 2 + margin.right - margin.left)
+      .attr("y", 30)
+      .attr("text-anchor", "middle")
+      .style("font-size", "26px")
+      .text("Doping in Professional Bicycle Racing");
 
-  d3.select("body").select("svg")
-    .append("g")
-    .attr("height", height - 50)
-    .attr("transform", "translate(70, -5)")
-    .call(leftAxis)
+  d3.select("svg") // y axis text
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("transform", "translate(30, 200)rotate(-90)")
+      .text("Ranking");
 
+  d3.select("svg") // x axis text
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("transform", "translate(425, 420)")
+      .text("Minutes Behind Fastest Time");
+
+  d3.select("svg") // legend circle
+      .append("circle")
+      .attr("cy", 250)
+      .attr("cx", 600)
+      .attr("r", 5)
+      .attr('fill', 'red')
+      .attr('stroke', 'black')
+
+  d3.select("svg") // legend circle
+      .append("circle")
+      .attr("cy", 300)
+      .attr("cx", 600)
+      .attr("r", 5)
+      .attr('fill', 'black')
+
+  d3.select("svg") // legend text for no doping
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("transform", "translate(685, 304)")
+      .style("font-size", '10px')
+      .text("Riders without doping allegations");
+
+  d3.select("svg") // legend text
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("transform", "translate(678, 254)")
+      .style("font-size", '10px')
+      .text("Riders with doping allegations");
+
+  d3.select("body").select("svg") // bottom axis
+      .append("g")
+      .attr("width", width)
+      .attr("transform", "translate(70, 375)")
+      .call(bottomAxis);
+
+  d3.select("body").select("svg") // left axis
+      .append("g")
+      .attr("height", height - 50)
+      .attr("transform", "translate(70, -5)")
+      .call(leftAxis);
 }
 createDataSet = function() {
   var data = [];
